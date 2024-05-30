8a21474e6cca1334644837931433091dd4e1763b
sonia francesconi <soniafra.93@gmail.com>
2024-05-30 20:38:04 +0100

    rentals controller, views, routes

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,4 +4,6 @@ class Rental < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :status, presence: true, inclusion: { in: %w[pending accepted declined] }
+
 end
