e53a803ed94040a0f5e21b2f79111f9182766ff6
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-06-04 19:16:03 +0100

    added typed.js text to the homepage, added rental path to the rental link in the navbar

---
+++ app/javascript/controllers/typed_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+// Connects to data-controller="typed"
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["Travel lightly with your family.", "No more heavy luggages!",
+      "Traveling made easy with Portable Parenting!"],
+      typeSpeed: 100,
+    })
+  }
+}
