fbe250438a293b8b7ff3547d1e402b6c2bbb2392
sonia francesconi <soniafra.93@gmail.com>
2024-06-06 10:28:34 +0100

    added sign in modal target and action

--- app/javascript/controllers/modal_sign_in_controller.js
+++ app/javascript/controllers/modal_sign_in_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modal-sign-in"
 export default class extends Controller {
+  static target = ["modal"];
+
   connect() {
-    console.log("Hello");
+    console.log("ModalSignInController connected");
+  }
+
+  fire() {
+    console.log("Button clicked!"); // Debug statement
+    this.modalTarget.classList.toggle("d-none");
+    console.log("Toggled visibility!"); // Debug statement
   }
 }
