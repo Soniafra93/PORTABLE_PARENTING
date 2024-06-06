23d84ebcfecbaf664c2c859cb6cc0ab16fac06e3
sonia francesconi <soniafra.93@gmail.com>
2024-06-06 22:20:24 +0100

    updated log in pop up

--- app/javascript/controllers/modal_sign_in_controller.js
+++ app/javascript/controllers/modal_sign_in_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modal-sign-in"
 export default class extends Controller {
-  static targets = ["modal"];
+  static targets = ["modal", "sign"];
 
   connect() {
     console.log("ModalSignInController connected");
@@ -13,6 +13,18 @@ export default class extends Controller {
 
     this.modalTarget.classList.toggle("d-none");
     if (this.modalTarget.classList.contains("d-none")) {
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+    console.log("Toggled visibility!"); // Debug statement
+  }
+
+  toggle() {
+    console.log("Button clicked!"); // Debug statement
+
+    this.signTarget.classList.toggle("d-none");
+    if (this.signTarget.classList.contains("d-none")) {
       document.body.style.overflow = "auto";
     } else {
       document.body.style.overflow = "hidden";
